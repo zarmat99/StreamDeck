16cacb7cae2bb7cef208205662fe8c7a5e01ea41
zarmat99 <mzarba99@gmail.com>
2023-12-03 20:39:00 +0100

    Formalizes the GUI code design.

--- App.py
+++ App.py
@@ -9,113 +9,172 @@ customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard),
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        self.a = 0
-        # ------------------- Connection Page -------------------------------------------------------------------------
-        # create page title label
-        self.connection_page_title_label = customtkinter.CTkLabel(self, text="Connection Page",
-                                                                  font=customtkinter.CTkFont(size=25, weight="bold"))
 
-        # create obs configurations frame
-        self.obs_conf_frame = customtkinter.CTkFrame(self)
-        self.obs_conf_frame.grid_rowconfigure((0, 1, 2, 3), weight=1)
-        self.obs_conf_frame.grid_columnconfigure((0, 1), weight=1)
-        #   row 0 -> frame label
-        self.obs_configuration_label = customtkinter.CTkLabel(self.obs_conf_frame, text="OBS Configurations",
-                                                 font=customtkinter.CTkFont(size=20))
-        self.obs_configuration_label.grid(row=0, column=0, columnspan=2)
-        #   row 1 -> host (label + entry)
-        #       host label
-        self.host_label = customtkinter.CTkLabel(self.obs_conf_frame, text="Host",
-                                                 font=customtkinter.CTkFont(size=15))
-        self.host_label.grid(row=1, column=0)
-        #       host entry
-        host_entry_var = tkinter.StringVar()
-        self.host_entry = customtkinter.CTkEntry(self.obs_conf_frame, textvariable=host_entry_var)
-        self.host_entry.grid(row=1, column=1)
-        #   row 2 -> port (label + entry)
-        #       port label
-        self.port_label = customtkinter.CTkLabel(self.obs_conf_frame, text="Port",
-                                                 font=customtkinter.CTkFont(size=15))
-        self.port_label.grid(row=2, column=0)
-        #       port entry
-        port_entry_var = tkinter.StringVar()
-        self.port_entry = customtkinter.CTkEntry(self.obs_conf_frame, textvariable=port_entry_var)
-        self.port_entry.grid(row=2, column=1)
-        #   row 3 -> password (label + entry)
-        #       password label
-        self.password_label = customtkinter.CTkLabel(self.obs_conf_frame, text="Password",
-                                                     font=customtkinter.CTkFont(size=15))
-        self.password_label.grid(row=3, column=0)
-        #       password entry
-        password_entry_var = tkinter.StringVar()
-        self.password_entry = customtkinter.CTkEntry(self.obs_conf_frame, textvariable=password_entry_var)
-        self.password_entry.grid(row=3, column=1)
-
-        # create streamdeck configuration frame
-        self.streamdeck_conf_frame = customtkinter.CTkFrame(self)
-        self.streamdeck_conf_frame.grid_rowconfigure((0, 1, 2), weight=1)
-        self.streamdeck_conf_frame.grid_columnconfigure((0, 1), weight=1)
-        #   row 0 -> frame label
-        self.obs_configuration_label = customtkinter.CTkLabel(self.streamdeck_conf_frame,
-                                                              text="StreamDeck Configurations",
-                                                              font=customtkinter.CTkFont(size=20))
-        self.obs_configuration_label.grid(row=0, column=0, columnspan=2)
-        #   row 1 -> com port (label + entry)
-        #       com port label
-        self.com_port_label = customtkinter.CTkLabel(self.streamdeck_conf_frame, text="Com Port",
-                                                     font=customtkinter.CTkFont(size=15))
-        self.com_port_label.grid(row=1, column=0)
-        #       com port entry
-        com_port_var = tkinter.StringVar()
-        self.com_port_entry = customtkinter.CTkEntry(self.streamdeck_conf_frame, textvariable=com_port_var)
-        self.com_port_entry.grid(row=1, column=1)
-        #   row 2 -> baud rate (label + entry)
-        #       baud rate label
-        self.baud_rate_label = customtkinter.CTkLabel(self.streamdeck_conf_frame, text="Baud Rate",
-                                                      font=customtkinter.CTkFont(size=15))
-        self.baud_rate_label.grid(row=2, column=0)
-        #       port entry
-        baud_rate_var = tkinter.StringVar()
-        self.baud_rate_entry = customtkinter.CTkEntry(self.streamdeck_conf_frame, textvariable=baud_rate_var)
-        self.baud_rate_entry.grid(row=2, column=1)
-
-        # create Connect Button
-        self.connect_button = customtkinter.CTkButton(self, text="Connect",
-                                                      command=self.connect_event)
-        # -------------------------------------------------------------------------------------------------------------
-
-        # ------------------- Page 2 ----------------------------------------------------------------------------------
-        self.back_button = customtkinter.CTkButton(self, text="Back",
-                                                      command=self.back_button_event)
+        # ------------------- Variables -------------------------------------------------------------------------------
+        self.page_2_title_label = None
+        self.page_2_title_frame = None
+        self.streamdeck_configuration_label = None
+        self.connection_page_bottom_frame = None
+        self.connection_page_title_frame = None
+        self.connect_button = None
+        self.baud_rate_entry = None
+        self.baud_rate_label = None
+        self.com_port_entry = None
+        self.com_port_label = None
+        self.obs_configuration_label = None
+        self.password_entry = None
+        self.password_label = None
+        self.port_entry = None
+        self.port_label = None
+        self.host_entry = None
+        self.host_label = None
+        self.streamdeck_conf_frame = None
+        self.obs_conf_frame = None
+        self.connection_page_title_label = None
+        self.host_entry_var = tkinter.StringVar()
+        self.port_entry_var = tkinter.StringVar()
+        self.com_port_entry_var = tkinter.StringVar()
+        self.password_entry_var = tkinter.StringVar()
+        self.baud_rate_var = tkinter.StringVar()
         # -------------------------------------------------------------------------------------------------------------
 
         # ------------------- Init ------------------------------------------------------------------------------------
-        self.connection_page_grid()
+        self.create_connection_page()
+        self.create_page_2()
+        self.grid_connection_page()
+        # -------------------------------------------------------------------------------------------------------------
+        self.back_button = customtkinter.CTkButton(self, text="Back", command=self.back_button_event)
 
-    def connection_page_grid(self):
+    # ----------------------------------------- M E T H O D S ---------------------------------------------------------
+
+    # ------------------- Connection Page Methods ---------------------------------------------------------------------
+    def create_connection_page(self):
+        print("ciao")
+        self.create_connection_page_frames()
+        self.create_connection_page_widgets_in_frames()
+        self.configure_connection_page_frames()
+        self.grid_connection_page_widgets_in_frames()
+
+    def create_connection_page_frames(self):
+        self.connection_page_title_frame = customtkinter.CTkFrame(self)
+        print(self.connection_page_title_frame)
+        self.obs_conf_frame = customtkinter.CTkFrame(self)
+        self.streamdeck_conf_frame = customtkinter.CTkFrame(self)
+        self.connection_page_bottom_frame = customtkinter.CTkFrame(self)
+
+    def create_connection_page_widgets_in_frames(self):
+        # connection_page_title_frame
+        self.connection_page_title_label = customtkinter.CTkLabel(self.connection_page_title_frame,
+                                                                  text="Connection Page",
+                                                                  font=customtkinter.CTkFont(size=25, weight="bold"))
+        # obs_conf_frame
+        self.obs_configuration_label = customtkinter.CTkLabel(self.obs_conf_frame, text="OBS Configurations",
+                                                              font=customtkinter.CTkFont(size=20))
+        self.host_label = customtkinter.CTkLabel(self.obs_conf_frame, text="Host",
+                                                 font=customtkinter.CTkFont(size=15))
+        self.host_entry = customtkinter.CTkEntry(self.obs_conf_frame, textvariable=self.host_entry_var)
+        self.port_label = customtkinter.CTkLabel(self.obs_conf_frame, text="Port",
+                                                 font=customtkinter.CTkFont(size=15))
+        self.port_entry = customtkinter.CTkEntry(self.obs_conf_frame, textvariable=self.port_entry_var)
+        self.password_label = customtkinter.CTkLabel(self.obs_conf_frame, text="Password",
+                                                     font=customtkinter.CTkFont(size=15))
+        self.password_entry = customtkinter.CTkEntry(self.obs_conf_frame, textvariable=self.password_entry_var)
+        # streamdeck_conf_frame
+        self.streamdeck_configuration_label = customtkinter.CTkLabel(self.streamdeck_conf_frame,
+                                                                     text="StreamDeck Configurations",
+                                                                     font=customtkinter.CTkFont(size=20))
+        self.com_port_label = customtkinter.CTkLabel(self.streamdeck_conf_frame, text="Com Port",
+                                                     font=customtkinter.CTkFont(size=15))
+        self.com_port_entry = customtkinter.CTkEntry(self.streamdeck_conf_frame, textvariable=self.com_port_entry_var)
+        self.baud_rate_label = customtkinter.CTkLabel(self.streamdeck_conf_frame, text="Baud Rate",
+                                                      font=customtkinter.CTkFont(size=15))
+        self.baud_rate_entry = customtkinter.CTkEntry(self.streamdeck_conf_frame, textvariable=self.baud_rate_var)
+        # connection_page_bottom_frame
+        self.connect_button = customtkinter.CTkButton(self.connection_page_bottom_frame, text="Connect",
+                                                      command=self.connect_event)
+
+    def configure_connection_page_frames(self):
+        # connection_page_title_frame
+        self.connection_page_title_frame.grid_rowconfigure(0, weight=1)
+        self.connection_page_title_frame.grid_columnconfigure(0, weight=1)
+        # obs_conf_frame
+        self.obs_conf_frame.grid_rowconfigure((0, 1, 2, 3), weight=1)
+        self.obs_conf_frame.grid_columnconfigure((0, 1), weight=1)
+        # streamdeck_conf_frame
+        self.streamdeck_conf_frame.grid_rowconfigure((0, 1, 2), weight=1)
+        self.streamdeck_conf_frame.grid_columnconfigure((0, 1), weight=1)
+        # connection_page_bottom_frame
+        self.connection_page_bottom_frame.grid_rowconfigure(0, weight=1)
+        self.connection_page_bottom_frame.grid_columnconfigure(0, weight=1)
+
+    def grid_connection_page_widgets_in_frames(self):
+        # connection_page_title_frame
+        self.connection_page_title_label.grid(row=0, column=0)
+        # obs_conf_frame
+        self.obs_configuration_label.grid(row=0, column=0, columnspan=2)
+        self.host_label.grid(row=1, column=0)
+        self.host_entry.grid(row=1, column=1)
+        self.port_label.grid(row=2, column=0)
+        self.port_entry.grid(row=2, column=1)
+        self.password_label.grid(row=3, column=0)
+        self.password_entry.grid(row=3, column=1)
+        # streamdeck_conf_frame
+        self.streamdeck_configuration_label.grid(row=0, column=0, columnspan=2)
+        self.com_port_label.grid(row=1, column=0)
+        self.com_port_entry.grid(row=1, column=1)
+        self.baud_rate_label.grid(row=2, column=0)
+        self.baud_rate_entry.grid(row=2, column=1)
+        # connection_page_bottom_frame
+        self.connect_button.grid(row=0, column=0, sticky="e")
+
+    def grid_connection_page(self):
         # configure window
         self.title("StreamDeck - Connection")
         self.geometry(f"{800}x{400}")
 
         # configure grid
         self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=0)
-        self.grid_rowconfigure((1, 2), weight=1)
-        self.grid_rowconfigure(3, weight=0)
+        self.grid_rowconfigure((0, 1, 2, 3), weight=1)
 
-        # grid
-        self.connection_page_title_label.grid(row=0, column=0, columnspan=2, pady=10)
-        self.obs_conf_frame.grid(row=1, column=0, columnspan=2, sticky="nswe", padx=20, pady=(0, 10))
-        self.streamdeck_conf_frame.grid(row=2, column=0, columnspan=2, sticky="nswe", padx=20, pady=(10, 10))
-        self.connect_button.grid(row=3, column=0, columnspan=2, padx=20, pady=(10, 20), sticky="e")
+        # grid frames
+        self.connection_page_title_frame.grid(row=0, column=0, sticky="nswe", padx=20, pady=(20, 5))
+        self.obs_conf_frame.grid(row=1, column=0, sticky="nswe", padx=20, pady=(5, 5))
+        self.streamdeck_conf_frame.grid(row=2, column=0,  sticky="nswe", padx=20, pady=(5, 5))
+        self.connection_page_bottom_frame.grid(row=3, column=0, sticky="nswe", padx=20, pady=(5, 20))
 
-    def connection_page_forget(self):
-        self.obs_conf_frame.grid_forget()
+    def forget_connection_page(self):
+        self.connection_page_title_frame.grid_forget()
         self.streamdeck_conf_frame.grid_forget()
-        self.connection_page_title_label.grid_forget()
-        self.connect_button.grid_forget()
+        self.obs_conf_frame.grid_forget()
+        self.connection_page_bottom_frame.grid_forget()
+    # -----------------------------------------------------------------------------------------------------------------
 
-    def page_2_grid(self):
+    # ------------------- Page 2 Methods ------------------------------------------------------------------------------
+    def create_page_2(self):
+        self.create_page_2_frames()
+        self.create_page_2_widgets_in_frames()
+        self.configure_page_2_frames()
+        self.grid_page_2_widgets_in_frames()
+
+    def create_page_2_frames(self):
+        self.page_2_title_frame = customtkinter.CTkFrame(self)
+
+    def configure_page_2_frames(self):
+        # page_2_title_frame
+        self.page_2_title_frame.grid_rowconfigure(0, weight=1)
+        self.page_2_title_frame.grid_columnconfigure(0, weight=1)
+
+    def create_page_2_widgets_in_frames(self):
+        # page_2_title_frame
+        self.page_2_title_label = customtkinter.CTkLabel(self.page_2_title_frame,
+                                                         text="Page 2",
+                                                         font=customtkinter.CTkFont(size=25, weight="bold"))
+
+    def grid_page_2_widgets_in_frames(self):
+        # page_2_title_frame
+        self.page_2_title_label.grid(row=0, column=0)
+
+    def grid_page_2(self):
         # configure window
         self.title("StreamDeck - Page 2")
         self.geometry(f"{800}x{400}")
@@ -125,18 +184,19 @@ class App(customtkinter.CTk):
         self.grid_rowconfigure(0, weight=1)
 
         # grid
-        self.back_button.grid(row=0, column=0)
+        self.page_2_title_frame.grid(row=0, column=0, sticky="nswe", padx=20, pady=(20, 5))
 
-    def page_2_forget(self):
-        self.back_button.grid_forget()
+    def forget_page_2(self):
+        self.page_2_title_frame.grid_forget()
+    # -----------------------------------------------------------------------------------------------------------------
 
     def connect_event(self):
-        self.connection_page_forget()
-        self.page_2_grid()
+        self.forget_connection_page()
+        self.grid_page_2()
 
     def back_button_event(self):
-        self.page_2_forget()
-        self.connection_page_grid()
+        self.forget_page_2()
+        self.grid_connection_page()
 
 
 if __name__ == "__main__":
